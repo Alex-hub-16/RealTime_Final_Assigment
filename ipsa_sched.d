07950b7e4ce0bcb8768d833445808569d2225aee
Alexandre <88715439+Alex-hub-16@users.noreply.github.com>
2024-01-14 23:33:36 +0100

    Add files via upload

---
+++ ipsa_sched.d
@@ -0,0 +1,21 @@
+build/ipsa_sched.o: ipsa_sched.c FreeRTOS.h FreeRTOSConfig.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcRecorder.h \
+ trcConfig.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcPortDefines.h \
+ trcSnapshotConfig.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcPortDefines.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcHardwarePort.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcKernelPort.h \
+ FreeRTOS.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/projdefs.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/portable.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/deprecated_definitions.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/portmacro.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/mpu_wrappers.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/task.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/list.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/timers.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/task.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/semphr.h \
+ /home/alex/RealTime/FreeRTOSv202107.00/FreeRTOS/Source/include/queue.h \
+ console.h
